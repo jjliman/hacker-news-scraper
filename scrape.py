7f45c9cef7128764b2f99ab491dbac838dbc06ac
Justin Liman <justinliman97@gmail.com>
2021-03-20 01:21:36 -0700

    get stories and votes

--- scrape.py
+++ scrape.py
@@ -4,4 +4,7 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
+links = soup.select('.storylink')
+votes = soup.select('.score')
+# for vote in votes:
+#     print(vote)
