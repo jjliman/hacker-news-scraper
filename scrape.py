c4bca6d11e927f46caaec0ea3a152abdca626386
Justin Liman <justinliman97@gmail.com>
2021-03-20 01:55:51 -0700

    get all posts with 100 or more upvotes

--- scrape.py
+++ scrape.py
@@ -1,10 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
+
+
+def create_custom_hn(links, subtexts):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtexts[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
 
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
-votes = soup.select('.score')
-# for vote in votes:
-#     print(vote)
+subtexts = soup.select('.subtext')
+
+
+pprint.pprint(create_custom_hn(links, subtexts))
